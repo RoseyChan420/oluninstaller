c8ebcfc9f22155a85bec46c602bfdb679caf630d
K0 <109837094+TotallyNotK0@users.noreply.github.com>
2025-02-10 15:27:57 -0600

    Delete run.bat

--- run.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-net session >nul 2>&1
-if %errorLevel% neq 0 (
-    powershell -Command "Start-Process '%~dpnx0' -Verb RunAs"
-    exit
-)
-powershell -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -File "%~dp0uninstall_oo.ps1"
-exit
